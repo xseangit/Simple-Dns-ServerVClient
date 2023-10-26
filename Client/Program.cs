2a6dcadb2af704b289f7652d450649bce493b323
alihagh1383 <xseanmail@gmail.com>
2023-10-26 09:53:30 +0330

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using DNS.Client;
+using DNS.Protocol;
+using DNS.Protocol.ResourceRecords;
+using System.Net;
+
+ClientRequest request = new ClientRequest("65.109.211.154");
+
+// Request an IPv6 record for the foo.com domain
+request.Questions.Add(new Question(Domain.FromString("google.com")));
+request.RecursionDesired = true;
+
+IResponse response = await request.Resolve();
+
+// Get all the IPs for the foo.com domain
+IList<IPAddress> ips = response.AnswerRecords
+    .Cast<IPAddressResourceRecord>()
+    .Select(r => r.IPAddress)
+    .ToList();
+
+foreach (IPAddress ip in ips)
+{
+    Console.WriteLine(ip.ToString());
+}
---
+++ dns/Program.cs
@@ -0,0 +1,19 @@
+﻿// Proxy to google's DNS
+using DNS.Server;
+
+MasterFile masterFile = new MasterFile();
+DnsServer server = new DnsServer(masterFile, "8.8.8.8");
+
+// Resolve these domain to localhost
+masterFile.AddIPAddressResourceRecord("google.com", "127.0.0.1");
+masterFile.AddIPAddressResourceRecord("github.com", "127.0.0.1");
+
+// Log every request
+server.Requested += (sender, e) => Console.WriteLine(e.Request);
+// On every successful request log the request and the response
+server.Responded += (sender, e) => Console.WriteLine("{0} => {1}", e.Request, e.Response);
+// Log errors
+server.Errored += (sender, e) => Console.WriteLine(e.Exception.Message);
+
+// Start the server (by default it listens on port 53)
+await server.Listen();
